bbb1cecc0c5f9c5cf8f04b1a2f3ed8936cff1de5
EiG123 <narinsuwan567@gmail.com>
2024-02-16 00:04:12 +0700

    Router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import Home from "./components/Home.js";
 import About from "./components/About.js";
 import Blogs from "./components/Blogs.js";
+import {BrowserRouter,Routes,Route} from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
-      <Home/>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+        <Route path="/about" element={<About/>}></Route>
+        <Route path="/blogs" element={<Blogs/>}></Route> 
+      </Routes>
+    </BrowserRouter>
   );
 }
 
