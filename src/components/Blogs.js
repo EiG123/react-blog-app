540119ef3361f253087e17c2c7bad93dd19b5d26
EiG123 <narinsuwan567@gmail.com>
2024-02-16 22:54:24 +0700

    search blog

--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -1,13 +1,29 @@
 import "./Blogs.css";
 import blogs from "../data/blogs";
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 function Blogs() {
+  const [search, setSearch] = useState("");
+  const [filterBlog, setFilterBlog] = useState([]);
+  useEffect(()=>{
+    const result = blogs.filter((item)=>item.title.includes(search));
+    setFilterBlog(result)
+  }, [search])
   return (
     <div className="blogs-container">
+      <div className="search-container">
+        <input
+          type="text"
+          className="search-input"
+          placeholder="ค้นหาบทความ"
+          value={search}
+          onChange={(e)=>setSearch(e.target.value)}
+        />
+      </div>
       <article>
-        {blogs.map((item) => (
+        {filterBlog.map((item) => (
           <Link to={`/blog/${item.id}`} key={item.id}>
-            <div className="card" key={item.id} >
+            <div className="card" key={item.id}>
               <h2>{item.title}</h2>
               <p>{item.content.substring(0, 300)}</p>
               <hr />
