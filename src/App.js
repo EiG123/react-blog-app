1bd5371c70ad1c2aecc93cd11a56402fada9fcc4
EiG123 <narinsuwan567@gmail.com>
2024-02-16 15:46:13 +0700

    NotFound page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import About from "./components/About.js";
 import Blogs from "./components/Blogs.js";
 import {BrowserRouter,Routes,Route} from "react-router-dom";
 import Navbar from "./components/Navbar.js";
+import NotFound from "./components/NotFound.js";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Home/>}></Route>
         <Route path="/about" element={<About/>}></Route>
         <Route path="/blogs" element={<Blogs/>}></Route> 
+        <Route path="*" element={<NotFound/>}></Route>
       </Routes>
     </BrowserRouter>
   );
---
+++ src/components/NotFound.js
@@ -0,0 +1,7 @@
+export default function NotFound(){
+    return(
+        <>
+        <h3>ไม่พบหน้าเว็บ (404 Page Not Found)</h3>
+        </>
+    );
+}
