491db20800407a81309bd6f71173d3eb2cb5fbc3
EiG123 <narinsuwan567@gmail.com>
2024-02-16 15:21:55 +0700

    navbar

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import Home from "./components/Home.js";
 import About from "./components/About.js";
 import Blogs from "./components/Blogs.js";
 import {BrowserRouter,Routes,Route} from "react-router-dom";
-
+import Navbar from "./components/Navbar.js";
 
 function App() {
   return (
     <BrowserRouter>
+    <Navbar/>
       <Routes>
         <Route path="/" element={<Home/>}></Route>
         <Route path="/about" element={<About/>}></Route>
